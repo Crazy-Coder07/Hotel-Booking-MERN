76cfcf8498e300b7f3b0e993d44a842515075061
Crazy-Coder07 <adityaranjan7479@gmail.com>
2023-05-25 14:40:22 +0530

    completed react-redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, combineReducers } from 'redux';
+
+import {Provider} from "react-redux"
+import {composeWithDevTools} from "redux-devtools-extension"
+
+import rootReducer from './reudcers';
+
+
+
+// create redux store 
+const store =createStore(rootReducer,composeWithDevTools( ));
+
+// provide redux store to the entire app
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+       <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
